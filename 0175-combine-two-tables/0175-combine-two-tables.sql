6ac7e6767b0abac4f76bf8ea9177a40e28ff2919
Tanvi Jain <tanvij.0072@gmail.com>
2025-06-29 13:59:55 +0530

    Time: 423 ms (42.52%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
-select Person.firstName, Person.lastName,Address.city, Address.state
-from Person
-left join Address
-on Address.personId=Person.personId; 
-
-#MySQL LEFT JOIN is a type of outer join that returns all records from the left table and matches records from the right table. If there is no match, the result is NULL from the right table. This join is also known as Left Outer Join.
+select p.firstName, p.lastName, a.city, a.state 
+from Person p
+left join address a 
+on p.personId=a.personId; 
